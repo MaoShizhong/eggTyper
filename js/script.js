52c3f088d8e4456a89e5c1e5cc466c433055931d
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-04-16 14:30:48 +0100

    Implement test time limit and reset button
    Resetting will remove the button, clear the input field and generate
    a new word list.
    
    Fixed generateWordList() to clear text display before loading words.
    Refactored generateWordList() - now more simply populates character
    list for score comparison.
    
    Text inputs will populate the comparison array - accounts for backspace
    and CTRL+A backspace. This array will be used to highlight the text
    area appropriately and also be compared to the word list for scoring.
    Also changed placeholder in case all input text backspaced mid-test.
    Instructions moved to where reset button is - typing/resetting will
    toggle between these.
    
    Typing initiates test which now triggers the timer.
    Upon test end, the results element will display.
    Clicking the reset button at any point during or after the test
    will reset the timer and clear any intervals/timeouts.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 import { words, shuffleWords } from './words.js';
 
 const themeBtn = document.querySelector('#theme');
+const instruction = document.querySelector('#caption');
+const resetBtn = document.querySelector('#reset');
+const textDisplay = document.querySelector('#text-display');
+const input = document.querySelector('#text-entry');
+const timer = document.querySelector('#timer');
+const results = document.querySelector('#results');
 const collapsibleBtn = document.querySelector('.collapsible');
 
-// global array to compare 
-const displayCharacters = [];
-const charactersToCheckAgainst = [];
+// global arrays to compare for scoring
+let charactersToCheckAgainst;
+let charactersBeingEntered = [];
+
+let duration;
+let clock;
+let testActive = false;
+let selectedDuration = 4;
+let testDurationInSeconds = selectedDuration;
 
 themeBtn.addEventListener('click', changeTheme);
+resetBtn.addEventListener('click', resetTest);
+input.addEventListener('input', startTest);
+input.addEventListener('input', updateTextDisplay);
 collapsibleBtn.addEventListener('click', showContent);
 
 function changeTheme() {
@@ -23,20 +38,76 @@ function showContent(e) {
         e.currentTarget.textContent = 'How is this calculated? \u25BC';
     }
     else {
-        collapsibleContent.style.maxHeight = collapsibleContent.scrollHeight + 'px';
+        collapsibleContent.style.maxHeight = `${collapsibleContent.scrollHeight}px`;
         e.currentTarget.textContent = 'How is this calculated? \u25B2';
     }
 }
 
-function generateWordList(displayChar, scoreChar) {
-    const textDisplay = document.querySelector('#text-display');
+function generateWordList() {
     shuffleWords(words);
-    displayChar = words.slice(0, 200).join(' ').split('');
-    for (let i = 0; i < displayChar.length; i++) {
-        textDisplay.textContent += `${displayChar[i]}`;
-        scoreChar[i] = displayChar[i];
+    const testWords = words.slice(0, 250).join(' ');
+    textDisplay.textContent = testWords;
+
+    // populate global array for scoring after test
+    charactersToCheckAgainst = testWords.split('');
+}
+
+function startTest() {
+    if (!testActive) {
+        testActive = true;
+        clock = setInterval(updateTimer, 1000);
+        duration = setTimeout(showResults, selectedDuration * 1000);
+        instruction.classList.add('hidden');
+        resetBtn.classList.remove('hidden');
     }
 }
 
+function updateTimer() {
+    testDurationInSeconds--;
+    timer.textContent = `${Math.floor(testDurationInSeconds / 60)}:${String(testDurationInSeconds % 60).padStart(2, '0')}`;
+}
+
+function updateTextDisplay(e) {
+    // to catch CTRL+A backspace or other complete clears
+    if (input.value === '') {
+        charactersBeingEntered = [];
+    }
+    else if (e.inputType === 'deleteContentBackward') {
+        charactersBeingEntered.pop();
+    }
+    else {
+        charactersBeingEntered.push(input.value.slice(-1));
+    }
+    console.log(charactersBeingEntered[charactersBeingEntered.length - 1]);
+
+    // highlightText();
+}
+
+function showResults() {
+    input.disabled = true;
+    clearInterval(clock);
+    timer.classList.toggle('hidden');
+    results.classList.toggle('hidden');
+}
+
+function resetTest() {
+    testDurationInSeconds = selectedDuration;
+    timer.textContent = `${Math.floor(testDurationInSeconds / 60)}:${String(testDurationInSeconds % 60).padStart(2, '0')}`;
+    testActive = false;
+    input.disabled = false;
+
+    generateWordList();
+    charactersToCheckAgainst = [];
+    charactersBeingEntered = [];
+    input.value = '';
+
+    timer.classList.remove('hidden');
+    results.classList.add('hidden');
+    instruction.classList.toggle('hidden');
+    resetBtn.classList.toggle('hidden');
+    clearTimeout(duration);
+    clearInterval(clock);
+}
+
 // initialise
-generateWordList(displayCharacters, charactersToCheckAgainst);
+generateWordList();
