9fe346a70440afe6285ef50f8fbd0779b9694934
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-01-23 22:09:49 +0000

    Implement input letter correctness logic and styling

--- src/components/words/Test.tsx
+++ src/components/words/Test.tsx
@@ -1,28 +1,64 @@
-import { useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import { TestType } from '../../types/types';
 import { Words } from './Words';
 import testStyles from './css/test.module.css';
 
 type TestProps = {};
 
+const words =
+    'Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords WordsWordsWords Words WordsWordsWords WordsWordsWords Words WordsWordsWords';
+
 export function Test({}: TestProps) {
     const [testType, setTestType] = useState<TestType>('words');
     const [fontSize, setFontSize] = useState(20); // ! Will need input/slider
     const [resetWordsStateOnNewTest, setResetWordsStateOnNewTest] = useState(Math.random());
+    const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
+    const [letterCorrectness, setLetterCorrectness] = useState<boolean[]>([]);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    function markLetterCorrectness(e: FormEvent<HTMLInputElement>): void {
+        const input = e.target as HTMLInputElement;
+
+        if (!input.value) {
+            setLetterCorrectness([]);
+        } else if (input.value.length < letterCorrectness.length) {
+            setLetterCorrectness(letterCorrectness.slice(0, input.value.length));
+        } else {
+            setLetterCorrectness([
+                ...letterCorrectness,
+                input.value.at(-1) === words[input.value.length - 1],
+            ]);
+        }
+    }
+
+    function resetTest(): void {
+        setResetWordsStateOnNewTest(Math.random());
+        setLetterCorrectness([]);
+
+        if (inputRef.current) inputRef.current.value = '';
+    }
 
     return (
         <section className={testStyles.test}>
             <h1>The timer will start when you begin typing below</h1>
             <Words
                 key={resetWordsStateOnNewTest}
+                words={words}
+                letterCorrectness={letterCorrectness}
                 fontSize={fontSize}
                 testType={testType}
-                resetState={setResetWordsStateOnNewTest}
             />
-            <input className={testStyles.input} type="text" placeholder="Start typing here" />
+            <input
+                className={testStyles.input}
+                type="text"
+                placeholder="Start typing here"
+                onInput={markLetterCorrectness}
+                ref={inputRef}
+            />
 
             {/* Change to actual reset when test behaviour implemented */}
-            <button onClick={(): void => setResetWordsStateOnNewTest(Math.random())}>reset</button>
+            <button onClick={resetTest}>reset</button>
         </section>
     );
 }
--- src/components/words/Words.tsx
+++ src/components/words/Words.tsx
@@ -2,13 +2,15 @@ import { TestType } from '../../types/types';
 import testStyles from './css/test.module.css';
 
 type WordsProps = {
+    words: string;
+    letterCorrectness: boolean[];
     fontSize: number;
     testType: TestType;
 };
 
 const ROWS = 5;
 
-export function Words({ fontSize, testType }: WordsProps) {
+export function Words({ words, letterCorrectness, fontSize, testType }: WordsProps) {
     const lineHeight = fontSize * 1.5;
     return (
         <div className={testStyles.words_container}>
@@ -19,13 +21,21 @@ export function Words({ fontSize, testType }: WordsProps) {
                     height: `${ROWS * lineHeight}px`,
                 }}
             >
-                Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words
-                WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords
-                Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words
-                WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords
-                Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words
-                WordsWordsWords Words WordsWordsWords Words WordsWordsWords Words WordsWordsWords
-                Words WordsWordsWords Words
+                {words.split('').map((letter: string, i: number): JSX.Element => {
+                    const currentLetter = letterCorrectness.length === i;
+                    const isScored = letterCorrectness.length >= i + 1;
+                    const correctness = letterCorrectness[i] ? 'correct' : 'wrong';
+
+                    const classes = [];
+                    if (currentLetter) classes.push(testStyles.current);
+                    if (isScored) classes.push(testStyles[correctness]);
+
+                    return (
+                        <span key={i} className={classes.join(' ')}>
+                            {letter}
+                        </span>
+                    );
+                })}
             </div>
         </div>
     );
