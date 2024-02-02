0199a6bcd2641fe6df964d7054ffecb591888a3f
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-02 08:03:35 +0000

    Convert word list into array of rows of characters

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -8,7 +8,7 @@ export function getWordBlock(testType: TestType): string {
 
 function shuffleInPlace<T>(arr: T[]): T[] {
     for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
+        const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     return arr;
@@ -37,4 +37,31 @@ export function formatTime({ time, withLetters }: { time: number; withLetters: b
 
 export function toMaxOneDP(num: number): number {
     return Math.round(num * 10) / 10;
+}
+
+export function toRows(words: string, rowMaxChars: number): string[][] {
+    let rowBreakpoint = rowMaxChars;
+
+    // hack for `.slice` in the last loop
+    const indicesOfRowEndSpaces = [-1];
+    let spaceIndex = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (i > rowBreakpoint) {
+            indicesOfRowEndSpaces.push(spaceIndex);
+            rowBreakpoint = spaceIndex + rowMaxChars;
+        } else if (words[i] === ' ') {
+            spaceIndex = i;
+        }
+    }
+
+    const rowedLetters: string[][] = [];
+    for (let i = 1; i < indicesOfRowEndSpaces.length; i++) {
+        const start = indicesOfRowEndSpaces[i - 1] + 1;
+        const end = indicesOfRowEndSpaces[i] + 1;
+        rowedLetters.push(words.slice(start, end).split(''));
+    }
+    // get last chars in (not enough to fill a whole row)
+    rowedLetters.push(words.slice(indicesOfRowEndSpaces.at(-1)).split(''));
+
+    return rowedLetters;
 }
