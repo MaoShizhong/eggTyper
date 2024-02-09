d11c746b22663aabc1b756671f60819da5f5641c
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-09 16:53:21 +0000

    Chore: Fix UI colour component state management
    Previous version required a hack that would not work in production
    due to strict mode, where a ref was used to prevent a useEffect from
    setting state on mount (only on update).
    Now this has been fixed so the intended behaviour is identical both in
    devlopment with strict mode and in production without.
    
    Updated README.

--- src/components/UI_customisation/ThemePicker.tsx
+++ src/components/UI_customisation/ThemePicker.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { Dispatch, SetStateAction, useRef } from 'react';
 import { SetThemeAction, THEMES, ThemeName } from '../../helpers/themes';
 import { CustomiserButton } from './CustomiserButton';
 import { UIOptionsDialog } from './UIOptionsDialog';
@@ -7,14 +7,19 @@ import styles from './css/UI_options.module.css';
 type ThemePickerProps = {
     currentTheme: ThemeName;
     setTheme: SetThemeAction;
+    setColourKey: Dispatch<SetStateAction<number>>;
 };
 
-export function ThemePicker({ currentTheme, setTheme }: ThemePickerProps) {
+export function ThemePicker({ currentTheme, setTheme, setColourKey }: ThemePickerProps) {
     const themePickerRef = useRef<HTMLDialogElement>(null);
     const buttonID = 'theme-picker-button';
 
     function changeTheme(newTheme: ThemeName): void {
         setTheme(newTheme);
+        setColourKey((prev) => prev + 1);
+        localStorage.setItem(`correct-colour`, THEMES[newTheme]['--correct']);
+        localStorage.setItem(`wrong-colour`, THEMES[newTheme]['--wrong']);
+
         themePickerRef.current?.close();
     }
 
--- src/components/UI_customisation/UIOptions.tsx
+++ src/components/UI_customisation/UIOptions.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { SetThemeAction, ThemeName } from '../../helpers/themes';
 import { CorrectnessColourPicker } from './CorrectnessColourPicker';
 import { FontSize } from './FontSize';
@@ -13,12 +13,33 @@ type UIOptionsProps = {
 };
 
 export function UIOptions({ theme, setTheme, fontSize, setFontSize }: UIOptionsProps) {
+    /**
+     * Intended behaviour is that the used colours (and preview colour swatches) for
+     * correctness indicators are taken from local storage, with the current theme colour
+     * as a fallback if none in local storage.
+     * Changing this colour manually will set the local storage colour and make changes
+     * to all necessary components on screen.
+     * Changing the theme should also overwrite the local storage and rendered colours
+     * to the new theme's colours.
+     *
+     * Without this key hack, either the colour picker components would require some
+     * hack for effects (when theme changes) to only set the new colour based on theme
+     * dependency on update (and not mount), which would lead to breaking in development
+     * with strict mode, or the components would not rerender when local storage values
+     * change (since no state changes would occur).
+     *
+     * This key hack combines both - manually changing the colours via the colour picker
+     * will change a state and change local storage values, and changing the theme will
+     * force a completel rerender of the colour components by changing their keys.
+     */
+    const [key, setKey] = useState(1);
+
     return (
         <div className={styles.options}>
             <FontSize fontSize={fontSize} setFontSize={setFontSize} />
-            <CorrectnessColourPicker correctness="correct" theme={theme} />
-            <CorrectnessColourPicker correctness="wrong" theme={theme} />
-            <ThemePicker currentTheme={theme} setTheme={setTheme} />
+            <CorrectnessColourPicker key={`correct-${key}`} correctness="correct" theme={theme} />
+            <CorrectnessColourPicker key={`wrong-${key}`} correctness="wrong" theme={theme} />
+            <ThemePicker currentTheme={theme} setTheme={setTheme} setColourKey={setKey} />
         </div>
     );
 }
