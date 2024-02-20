6e2eba0ad24768cfb3663386bd50308489ec1b57
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-02-20 23:48:51 +0000

    Simplify useTheme hook
    Custom `SetThemeAction` function not needed. Since it only deals with
    side effects, `useEffect` would be more semantic and cleaner to write.
    End behaviour in app is identical.

--- src/helpers/hooks.ts
+++ src/helpers/hooks.ts
@@ -1,37 +1,24 @@
 import { Dispatch, RefObject, SetStateAction, useEffect, useLayoutEffect, useState } from 'react';
 import { DEFAULT_FONT_SIZE, DEFAULT_THEME } from './constants';
-import { SetThemeAction, THEMES, ThemeName } from './themes';
+import { THEMES, ThemeName } from './themes';
 import { getContentWidth } from './util';
 
-type ThemeSetter = [ThemeName, SetThemeAction];
+type ThemeSetter = [ThemeName, Dispatch<SetStateAction<ThemeName>>];
 type FontSizeSetter = [number, Dispatch<SetStateAction<number>>];
 
 export function useTheme(): ThemeSetter {
     const localStorageTheme = localStorage.getItem('theme') as ThemeName | null;
     const [currentTheme, setCurrentTheme] = useState(localStorageTheme ?? DEFAULT_THEME);
 
-    function setTheme(theme: ThemeName): void {
-        const root = document.querySelector<HTMLHtmlElement>(':root');
+    useEffect((): void => {
+        localStorage.setItem('theme', currentTheme);
 
-        if (!root) {
-            throw new Error(
-                'There is no <html> element. This would be a very unlikely occurrence.'
-            );
+        for (const [property, value] of Object.entries(THEMES[currentTheme])) {
+            document.documentElement.style.setProperty(property, value);
         }
+    }, [currentTheme]);
 
-        for (const [property, value] of Object.entries(THEMES[theme])) {
-            root.style.setProperty(property, value);
-        }
-
-        if (theme !== currentTheme) {
-            setCurrentTheme(theme);
-            localStorage.setItem('theme', theme);
-        }
-    }
-
-    setTheme(currentTheme);
-
-    return [currentTheme, setTheme];
+    return [currentTheme, setCurrentTheme];
 }
 
 export function useRowCapacity(
